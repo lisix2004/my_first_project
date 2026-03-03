672905b7c37160729196a168ca6b81fd9b27813c
lisix2004 <lisix2004@gmail.com>
2026-03-04 00:31:57 +0800

    add douban_rank_rating

--- spider/douban_top250.py
+++ spider/douban_top250.py
@@ -1,10 +1,11 @@
-# 导入两个库
 import re
 import requests
 
-# 请求头，地址
-headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:148.0) Gecko/20100101 Firefox/148.0"}
-url = "https://movie.douban.com/top250"
+# 定义请求头，地址
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:148.0) Gecko/20100101 Firefox/148.0"
+}
+base_url = "https://movie.douban.com/top250"
 
 # 定义抓取网页函数
 def fetch_page(url):
@@ -14,17 +15,22 @@ def fetch_page(url):
 # 定义匹配函数
 def parse_page(html):
     pattern = re.compile(
-            r'<span class="title">([^&nbsp].*?)</span>')
+        r'<span class="title">(.*?)</span>.*?'  # 匹配电影名称
+        r'<span class="rating_num" property="v:average">(.*?)</span>',  # 匹配评分
+        re.DOTALL
+    )
     movies = pattern.findall(html)
     return movies
 
-html = fetch_page(url)
-movies = parse_page(html)
-
-# 抓取每一页
+# 爬取每一页
 for page in range(0, 250, 25):
-    url = f"https://movie.douban.com/top250?start={page}"
+    url = f"{base_url}?start={page}"
     html = fetch_page(url)
     movies = parse_page(html)
-    for movie in movies:
-        print(f"{movie}")
+
+    # 打印结果
+    for index, movie in enumerate(movies, start=page + 1):
+        title = movie[0].strip()  # 去除空格
+        rating = movie[1].strip()
+        print(f"排名: {index}, 电影名称: {title}, 评分: {rating}")
+
